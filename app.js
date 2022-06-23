243df55b73f28c5736caec02743a57ab2e6abfb1
Noah Ispas <noi@dataone.de>
2022-06-23 09:04:11 +0200

    add log

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
+  console.log("entered endpoint")
   let responseMessage = process.env.RESPONSE_MESSAGE || "Default Response Message";
   res.send(responseMessage);
 });
