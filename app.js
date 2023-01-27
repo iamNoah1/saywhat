76e2b079d7775225219ec5ae07d99e212d9370ac
Noah Ispas <noi@dataone.de>
2023-01-27 11:34:01 +0000

    craft v1, add values for dev and prod, include version in response, docker pushes include version

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const port = (process.env.PORT || 3000);
 const express = require('express');
 const app = express();
 
+const version = require('./package.json')
+
 app.get('/hello', function (req, res) {
   console.log("entered endpoint")
-  let responseMessage = process.env.RESPONSE_MESSAGE || "Default Response Message";
+  let responseMessage = (process.env.RESPONSE_MESSAGE || "Default Response Message") + " " + "v: " + version.version;
   res.send(responseMessage);
 });
 
